0ed5a7b6a228bdce00b29c18b8e43926a240fc81
Victor Arowo <arowov@gmail.com>
2019-09-06 08:20:52 +0100

    Created Header Component and Appended it to DOM

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,25 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    const div = document.createElement("div");
+    const dateSpan = document.createElement("span");
+    const h1 = document.createElement("h1");
+    const tempSpan = document.createElement("span");
+
+    div.classList.add("header");
+    dateSpan.classList.add("date");
+    tempSpan.classList.add("temp");
+
+    dateSpan.textContent = "MARCH 28, 2019";
+    h1.textContent = "Lambda Times";
+    tempSpan.textContent = "98°";
+
+    div.append(dateSpan);
+    div.append(h1);
+    div.append(tempSpan);
+
+    return div;
+}
+
+document.querySelector(".header-container").append(Header());
