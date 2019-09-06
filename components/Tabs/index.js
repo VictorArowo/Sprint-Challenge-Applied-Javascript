9505e02d49c3e43cea2d7077ac01212a661c1cba
Victor Arowo <arowov@gmail.com>
2019-09-06 08:38:03 +0100

    Got Topics from Axios Requests and Appended it to the DOM

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,14 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(res => {
+        res.data.topics.forEach(topic => {
+            let div = document.createElement("div");
+            div.classList.add("tab");
+            div.textContent = topic;
+
+            document.querySelector(".topics").append(div);
+        })
+    });
